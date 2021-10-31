4efa6bd18096acd129a9ddbc4717ac0615b15e31
Agniprdn09 <91820859+Agniprdn09@users.noreply.github.com>
2021-10-31 20:45:29 +0545

    Create add_sub.rs

---
+++ agnipradn09/add_sub.rs
@@ -0,0 +1,14 @@
+fn add(lhs: u32, rhs: u32) -> u32 {
+  let sum = lhs + rhs;
+   sum
+}
+
+fn sub(big: u32, small: u32) -> u32 {
+  let diff = big - diff;
+  diff
+}
+
+fn main() {
+  println!("Sum of 10 and 20 is {}", sum(10, 20));
+  println!("Difference of 20 and 10 is {}", diff(20, 10));
+}
